5bf5e3fd1222ae6e5f049bcddcc26932e8e65676
{Ximoto} <{orela.rec@gmail.com}>
2023-01-17 01:19:50 +0900

    linter追加

---
+++ vitest-react-sample/.eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:react/recommended',
+    // "standard-with-typescript" // standard スタイルガイドは使わないので消す
+    'airbnb', // 追加
+    'airbnb-typescript', // 追加
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+  },
+};
